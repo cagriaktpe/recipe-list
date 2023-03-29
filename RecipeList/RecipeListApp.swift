859f72994fb7d70bef7c46b1b9f690207d7310a8
Samet Çağrı Aktepe <sametcagriaktepe@gmail.com>
2023-03-29 16:00:33 +0200

    Initial Commit

---
+++ RecipeList/RecipeListApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeListApp.swift
+//  RecipeList
+//
+//  Created by Samet Cagri Aktepe on 29/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct RecipeListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
